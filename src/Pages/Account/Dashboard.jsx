f100458fc833756926950ecbc4036d1ccf9010be
slovetech619 <slovetech619@gmail.com>
2024-01-25 12:16:40 +0100

    fixing issues

--- src/Pages/Account/Dashboard.jsx
+++ src/Pages/Account/Dashboard.jsx
@@ -30,10 +30,10 @@ const Dashboard = () => {
                 </div>
                 <div className="grid grid-cols-1 lg:grid-cols-2 gap-3 lg:gap-4 lg:col-span-3">
                     <div className="bg-white rounded-lg px-4 py-6 grid grid-cols-2 gap-3">
-                        <div className="bg-red-400/30 text-red-500 border border-red-500 w-fit h-fit p-3 rounded-xl text-5xl"> <SlWallet /> </div>
+                        <div className="bg-red-400/30 text-red-500 border border-red-500 w-fit h-fit p-3 rounded-xl text-3xl"> <SlWallet /> </div>
                         <div className="">
-                            <div className="text-right font-bold text-2xl text-red-500 break-words">{NairaSign}500,000,000</div>
-                            <div className="font-semibold text-right text-zinc-500">Postpaid Balance</div>
+                            <div className="text-right font-bold text-lg text-red-500 break-words">{NairaSign}500,000,000</div>
+                            <div className="font-semibold text-right text-zinc-500 text-sm">Postpaid Balance</div>
                             <div className="w-fit ml-auto mt-5">
                                 <div className="relative w-32 h-2 rounded-full bg-zinc-300">
                                     <div className="bg-absolute top-0 left-0 w-4/5 h-2 rounded-full bg-red-400"></div>
@@ -42,10 +42,10 @@ const Dashboard = () => {
                         </div>
                     </div>
                     <div className="bg-white rounded-lg px-4 py-6 grid grid-cols-2 gap-3">
-                        <div className="bg-green-400/30 text-green-500 border border-green-500 w-fit h-fit p-3 rounded-xl text-5xl"> <BsHourglassSplit /> </div>
+                        <div className="bg-green-400/30 text-green-500 border border-green-500 w-fit h-fit p-3 rounded-xl text-3xl"> <BsHourglassSplit /> </div>
                         <div className="">
-                            <div className="text-right font-bold text-2xl text-green-500">30</div>
-                            <div className="font-semibold text-right text-zinc-500">Average Aging</div>
+                            <div className="text-right font-bold text-lg text-green-500">30</div>
+                            <div className="font-semibold text-right text-zinc-500 text-sm">Average Aging</div>
                             <div className="w-fit ml-auto mt-5">
                                 <div className="relative w-32 h-2 rounded-full bg-zinc-300">
                                     <div className="bg-absolute top-0 left-0 w-2/5 h-2 rounded-full bg-green-400"></div>
