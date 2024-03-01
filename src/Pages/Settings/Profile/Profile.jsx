199ee79e59a7086fef33988648cf84cf0ad2c948
slovetech619 <slovetech619@gmail.com>
2024-03-01 15:23:29 +0100

    feature: user settings

--- src/Pages/Settings/Profile/Profile.jsx
+++ src/Pages/Settings/Profile/Profile.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import SettingsLayout from '../SettingsLayout'
-import { SlCheck } from 'react-icons/sl'
+import { SlArrowDown, SlCheck } from 'react-icons/sl'
 import { useSelector } from 'react-redux'
 import EditProfile from './EditProfile'
 import { ImUser } from 'react-icons/im'
 import { FirebaseImage } from '/src/components/functions'
+import { AlertError } from '/src/components/functions'
 
 const Profile = () => {
     const [view, setView] = useState(false)
@@ -19,6 +20,15 @@ const Profile = () => {
             </div>
         )
     }
+    useEffect(() => {
+        const FetchOrg = async () => {
+            try {
+                
+            } catch (error) {
+                AlertError(`${error.message}`)
+            }
+        }
+    }, [])
     return (
         <SettingsLayout>
             {view && <UpdatePasspwordModal closeView={() => setView(!view)} />}
@@ -34,20 +44,21 @@ const Profile = () => {
                         <div onClick={() => setProf(!prof)} className="px-4 py-2 text-xs text-center text-white capitalize rounded-lg cursor-pointer bg-mainblue w-fit">edit profile</div>
                     </div>
                 </div>
-                <div className="w-11/12 py-8 ml-auto">
-                    <div className="flex items-center justify-between p-3 mb-4 border rounded-lg lg:w-4/6 bg-sky-50">
+                <div className="w-11/12 py-8 mx-auto">
+                    <div className="flex items-center justify-between p-3 mb-4 border rounded-lg bg-sky-50">
                         <div className="w-full">
                             <div className="capitalize text-slate-500">email address</div>
                             <div className="font-semibold">{user.email}</div>
                         </div>
                         <div className="text-blue-600"> <SlCheck /> </div>
                     </div>
-                    <div onClick={() => setView(!view)} className="flex items-center justify-between p-3 mb-4 bg-white border rounded-lg cursor-pointer lg:w-4/6">
+                    <div onClick={() => setView(!view)} className="flex items-center justify-between p-3 mb-4 bg-white border rounded-lg cursor-pointer">
                         <div className="">
                             <div className="capitalize text-slate-500">password</div>
                             <div className="font-semibold"> {passworddots()} </div>
                         </div>
                     </div>
+                    <div className="ml-auto w-fit"> <button className='flex items-center gap-2 text-mainblue'>View SLA Document <SlArrowDown /> </button> </div>
                 </div>
             </div>
         </SettingsLayout>
